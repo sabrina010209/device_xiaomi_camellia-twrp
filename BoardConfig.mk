bbd76341769ce9b0c59b0ec0dd7cedf7d45bfd60
Sushrut1101 <guptasushrut@gmail.com>
2024-03-15 15:33:11 -0500

    camellia: Set ``TW_NO_FASTBOOT_BOOT`` to true
    * Our device does not support hotbooting an image using fastboot boot,
      so `twrpfastboot=1` cmdline cause various issues like battery level
      detection, unable to reboot to system etc. When this flag is set to
      true, twrpfastboot=1 isn't added into the kernel cmdline and therefore
      fixes those issues.
    
    Change-Id: I67872bff2a80fe59ea886cfcdf555debf775eef7
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,6 +103,7 @@ TW_INCLUDE_REPACKTOOLS := true
 TW_INCLUDE_FASTBOOTD := true
 TW_HAS_NO_RECOVERY_PARTITION := true
 TW_EXCLUDE_APEX := true
+TW_NO_FASTBOOT_BOOT := true
 
 # Device
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
