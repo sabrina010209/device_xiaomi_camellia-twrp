cddc7247f18dd5f3e0c42f9182d7e74b84acc256
Sushrut1101 <guptasushrut@gmail.com>
2024-03-15 15:28:15 -0500

    camellia: Declare the VNDK version
    * Needed for decryption to work
    
    Change-Id: Ib362a923e6e0bbd157a87566fbe200096d859e28
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -43,6 +43,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 31
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
